4b793c5226913f783fe07b3799144a216ef3e327
Skyler.Ab <abrishami.bardia@gmail.com>
2025-04-08 19:56:42 -0400

    user import history settings added

--- apps/www/src/app/settings/import/page.tsx
+++ apps/www/src/app/settings/import/page.tsx
@@ -1,8 +1,23 @@
 import Link from "next/link";
 import React from "react";
 import HistoryUploader from "../_components/import/HistoryUploader";
+import { db } from "@workspace/database/connection";
+import { getCurrentSession } from "@/auth/session";
+import { redirect } from "next/navigation";
+import { cn } from "@workspace/ui/lib/utils";
 
-export default function Page() {
+const f = new Intl.DateTimeFormat();
+
+export default async function Page() {
+  const { session, user } = await getCurrentSession();
+
+  if (!session || !user) redirect("/login");
+
+  const exports = await db.query.userExports.findMany({
+    where: (fields, { eq }) => eq(fields.userId, user.id),
+    orderBy: (fields, { asc }) => asc(fields.userId),
+  });
+
   return (
     <div>
       <div className="mb-5 space-y-2">
@@ -20,6 +35,30 @@ export default function Page() {
         </p>
       </div>
       <HistoryUploader />
+      <div className="mt-5">
+        {exports.map((userExport) => (
+          <div key={userExport.exportId}>
+            <div className="flex items-center gap-5">
+              <h3 className="text-xl font-semibold">
+                {userExport.fileName.split("/").reverse()[0]}
+              </h3>
+              <div
+                className={cn("px-3 py-0.5 rounded-full", {
+                  "bg-yellow-500/30 text-yellow-600":
+                    userExport.status === "queued",
+                  "bg-green-500/30 text-green-600":
+                    userExport.status === "completed",
+                })}
+              >
+                {userExport.status}
+              </div>
+            </div>
+            <p className="text-sm text-muted-foreground mt-1">
+              Uploaded on {f.format(userExport.uploadDate as Date)}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
