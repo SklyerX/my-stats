bb4dcc2ea68b500d402273ca7355b0c41ef87fb0
Skyler.Ab <abrishami.bardia@gmail.com>
2025-05-10 18:22:27 -0400

    Update Spotify callback URL to use dynamic getUrl function

--- apps/www/src/auth/oauth.ts
+++ apps/www/src/auth/oauth.ts
@@ -2,9 +2,10 @@ import "server-only";
 
 import { Spotify } from "arctic";
 import { env } from "../env";
+import { getUrl } from "@/lib/utils";
 
 export const spotify = new Spotify(
   env.SPOTIFY_CLIENT_ID,
   env.SPOTIFY_CLIENT_SECRET,
-  "http://stats.skylerx.ir:3000/api/auth/spotify/callback",
+  `${getUrl()}/api/auth/spotify/callback`,
 );
